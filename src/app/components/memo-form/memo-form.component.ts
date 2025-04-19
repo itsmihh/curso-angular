5c843279d4700bf45a1db5253c04d2c3bd171550
milena <milenacsilva013@gmail.com>
2025-04-19 12:33:30 -0300

    Aula 28
    validação do formulário

--- src/app/components/memo-form/memo-form.component.ts
+++ src/app/components/memo-form/memo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-memo-form',
@@ -8,5 +9,32 @@ import { Component, Input } from '@angular/core';
 })
 export class MemoFormComponent {
   @Input() btnText!: string;
+
+  memoForm!: FormGroup
+
+  ngOnInit():void {
+    this.memoForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title() {
+    return this.memoForm.get('title')!;
+  }
+
+  get description() {
+    return this.memoForm.get('description')!;
+  }
+
+
+  submit() {
+    if(this.memoForm.invalid) {
+      return
+    }
+    console.log("Enviou o formulário");
+  }
 
 }
