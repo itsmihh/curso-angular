d6d4fbcdd7c19400daaa52f9a6c6600c925e4868
milena <milenacsilva013@gmail.com>
2025-04-22 12:04:56 -0300

    Aula 36
    - CSS página de memos

--- src/app/components/pages/memo/memo.component.ts
+++ src/app/components/pages/memo/memo.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MemoService } from '../../../services/memo.service';
 import { Moment } from '../../../Moment';
+import { environment } from '../../../../environments/environment';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-memo',
@@ -11,6 +13,9 @@ import { Moment } from '../../../Moment';
 })
 export class MemoComponent {
   memo?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private memoService: MemoService, private route: ActivatedRoute) {}
 
